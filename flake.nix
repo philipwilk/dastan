f4a78b6411040ad995a2e7719613ac3bcb6d6561
Philip Wilk <wiryfuture@gmail.com>
2023-04-28 12:27:10 +0100

    pain

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "csharp dotnet 8 flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            dotnet-sdk_8
+            dotnetPackages.Nuget
+          ];
+        };
+      }
+    );
+}
