b283ee8bed939f159d08c712f0dce6dfa86e140d
Philip Wilk <wiryfuture@gmail.com>
2023-06-11 23:50:22 +0100

    latest dotnet ver

--- flake.nix
+++ flake.nix
@@ -5,16 +5,18 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             dotnet-sdk_8
